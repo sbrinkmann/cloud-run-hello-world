a408ed2c38c5f8a9293a11535d17d9822e36bdae
Sascha Brinkmann <sascha.brinkmann@gmail.com>
2022-04-15 16:41:37 +0200

    Hallo Welt in deutsch

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hallo Welt, ich bin da \o/!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
